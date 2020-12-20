b671e603f40b12b2dfe4b38af04caba18a80d0fc
Garnik168 <64793477+Garnik168@users.noreply.github.com>
2020-12-19 22:51:40 -0500

    Uploading homework 3_1 and 3_1

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -1,31 +1,61 @@
 # Enter two numbers. If the first one is greater than the second, save first number in result_1,
 # otherwise save the second number to the result_1 variable.
 
-first_number = None
-second_number = None
+first_number = float(input('Enter Your First number for comparison: '))
+second_number = float(input('Enter Your Second number for comparison: '))
 result_1 = None
+if first_number > second_number:
+    result_1 = first_number
+    print(result_1)
+else:
+    result_1 = second_number
+    print(result_1)
 
 # Enter a random number in number_1 variable. If this number is 20 or
 # higher save “Too high” text to result_2, otherwise save “Thank you”.
-number_1 = None
+
+number_1 = float(input('Enter a random number please: '))
 result_2 = None
+if number_1 >= 20:
+    result_2 = 'Too High'
+    print(result_2)
+else:
+    result_2 = 'Tank you'
+    print(result_2)
 
 
 # Enter your first name and last name in first_name and last_name variables. If the length of your first name is under
 # five characters, join them together (without a space) and save it to result_3 variable in upper case. If the length
 # of the first name is five or more characters, save their first name in lower case in result_3 variable.
 
-first_name = None
-last_name = None
+first_name = input('Please enter Your First Name: ')
+last_name = input('Please enter Your Last Name: ')
 result_3 = None
-
+if len(first_name) < 5:
+    result_3 = first_name + last_name
+    result_3 = result_3.upper()
+    # print(result_3.upper())
+    print(result_3) # remove if the print is out of the IF statement
+else:
+    result_3 = first_name + ' ' + last_name
+    result_3 = result_3.lower()
+    # print(result_3.lower())
+    print(result_3) # remove if the print is out of the IF statement
+# print(result_3) # or just like that
 
 # Enter a number between 10 and 20 (inclusive) and save number to number_2 variable
 # If they enter a number within this range, save a message “Thank you” to result_4, otherwise a
 # message “Incorrect answer” to result_4.
 
-number_2 = None
+number_2 = float(input('Please enter a number between 10 and 20 (inclusive): '))
 result_4 = None
+if 10 <= number_2 <= 20:
+    result_4 = 'Thank you'
+    # print(result_4)
+else:
+    result_4 = 'Incorrect answer'
+    # print(result_4)
+print(result_4) # or here or repeat after each statement to see printed results
 
 
 # Enter your age. If you are 18 or over, save the message “You can vote” in result_5,
@@ -33,13 +63,72 @@ result_4 = None
 # if you are 16, save the message “You can buy a lottery ticket” in result_5,
 # if you are under 16, save the message “You can go Trick-or-Treating” in result_5 variable.
 
-age = None
+age = int(input('Enter your age: '))
 result_5 = None
+if age >= 18:
+    result_5 = 'You can vote'
+    print(result_5) # for every condition separate or one but outside of the conditions
+elif age == 17:
+    result_5 = 'You can learn to drive'
+    print(result_5) # for every condition separate or one but outside of the conditions
+elif age == 16:
+    result_5 = 'You can buy a lottery ticket'
+    print(result_5) # for every condition separate or one but outside of the conditions
+elif age < 16:
+    result_5 = 'You can go Trick-or-Treating'
+    print(result_5) # for every condition separate or one but outside of the conditions
+# or hash the last elif statement and do else instead
+# else:
+#     result_5 = 'You can go Trick-or-Treating'
+#     print(result_5) # for every condition separate or one but outside of the conditions
+# print(result_5) # or this at the end outside of the conditions or every condition separate
 
 
 # Enter a number between 1 and 12, save this value to month variable. Find which month is it.
 # (January, February, March, April, May, June, Jule, August, September, October, November, December)
 # Write answer in result_month in lower case
 
-month = None
+month = int(input('Enter a number between 1 and 12: '))
 result_month = None
+if month == 1:
+    result_month = 'January'
+    result_month = result_month.lower()
+    print(f'{month}\'st month is {result_month}')
+elif month == 2:
+    result_month = 'February'
+    result_month = result_month.lower()
+    print(f'{month}\'nd month is {result_month}')
+elif month == 3:
+    result_month = 'March'
+    result_month = result_month.lower()
+    print(f'{month}\'rd moth is {result_month}')
+elif month == 4:
+    result_month = 'April'
+    result_month = result_month.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 5:
+    result_month = 'May'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 6:
+    result_month = 'June'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 7:
+    result_month = 'July'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 8:
+    result_month = 'August'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 9:
+    result_month = 'September'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 10:
+    result_month = 'October'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 11:
+    result_month = 'November'.lower()
+    print(f'{month}\'th month is {result_month}')
+elif month == 12:
+    result_month = 'December'.lower()
+    print(f'{month}\'th month is {result_month}')
+else:
+    print('Incorrect input, try again. Hint: you should enter any whole number from 1 to 12 (included)')
--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -5,8 +5,17 @@
 # If number is  greater than 15 and less than or equal to 20, save the number raised to the power 3 to result_1
 # Else save the text "Wrong value" to result_1
 
-number = None
+number = int(input('Enter a number between 1 and 20: '))
 result_1 = None
+if 0 < number <= 7:
+    result_1 = number * 10
+elif 7 < number <= 15:
+    result_1 = number // 3
+elif 15 < number <= 20:
+    result_1 = number ** 3
+else:
+    result_1 = 'Wrong value, hint: enter a number between 1 and 20 (included) next time'
+print(result_1)
 
 
 # Enter two numbers between 1 and 10, save this values to number_1 variable and number_2 variables.
@@ -17,18 +26,71 @@ result_1 = None
 # If both numbers are greater than 5 and less than or equal to 10, multiply their sum by 3 and save it to result_2
 # Else save the text "Wrong values, try again" to result_2
 
-number_1 = None
-number_2 = None
+number_1 = int(input('Enter the first number between 1 and 10: '))
+number_2 = int(input('Enter the second number between 1 and 10: '))
 result_2 = None
+
+# SOLUTION 1
+
+# if 0 < number_1 <= 5 and 0 < number_2 <= 5:
+#     result_2 = number_1 * number_2
+# elif 5 < number_1 <= 10 and 5 < number_2 <= 10:
+#     result_2 = (number_1 + number_2) * 3
+# elif 5 < number_1 <= 10 or 5 < number_2 <= 10:
+#     result_2 = number_1 + number_2
+# else:
+#     result_2 = "Wrong values, try again. (Hint: Enter any random number in the range from 1 to 10 (included)"
+# print(result_2)
+
+# SOLUTION 2
+
+# if 5 < number_1 <= 10 and 5 < number_2 <= 10:
+#     result_2 = (number_1 + number_2) * 3
+# elif 5 < number_1 <= 10 or 5 < number_2 <= 10:
+#     result_2 = number_1 + number_2
+# elif 0 < number_1 <= 5 and 0 < number_2 <= 5:
+#     result_2 = number_1 * number_2
+# else:
+#     result_2 = "Wrong values, try again. (Hint: Enter any random number in the range from 1 to 10 (included)"
+# print(result_2)
+
+# SOLUTION 3
+# if 0 < number_1 <= 5 and 0 < number_2 <= 5:
+#     result_2 = number_1 * number_2
+# elif 0 < number_1 <= 5 and 5 < number_2 <= 10 or 5 < number_1 <= 10 and 0 < number_2 <= 5:
+#     result_2 = number_1 + number_2
+# elif 5 < number_1 <= 10 and 5 < number_2 <= 10:
+#     result_2 = (number_1 + number_2) * 3
+# else:
+#     result_2 = "Wrong values, try again. (Hint: Enter any random number in the range from 1 to 10 (included)"
+# print(result_2)
+
+# SOLUTION 4
+
+if 0 < number_1 <= 5 and 0 < number_2 <= 5:
+    result_2 = number_1 * number_2
+elif 5 < number_1 <= 10 and not 5 < number_2 <= 10 or 5 < number_2 <= 10 and not 5 < number_1 <= 10:
+    result_2 = number_1 + number_2
+elif 5 < number_1 <= 10 and 5 < number_2 <= 10:
+    result_2 = (number_1 + number_2) * 3
+else:
+    result_2 = "Wrong values, try again. (Hint: Enter any random number in the range from 1 to 10 (included)"
+print(result_2)
 
 # Enter your first name and save it to first_name variable,
 # then Enter last name and save it to last_name
 # If first_name or last_name are shorter than 6 characters, save a full name (with a space between) to result_3
 # Else save first_name to result_3 as many times as length of last_name value
 
-first_name = None
-last_name = None
+first_name = input('Please enter your First Name: ')
+last_name = input('Please enter your Last Name: ')
 result_3 = None
+if len(first_name) < 6 or len(last_name) < 6:
+    result_3 = f'{first_name} {last_name}'
+    # result_3 = first_name + ' ' + last_name #ANOTHER OPTION
+else:
+    result_3 = first_name * len(last_name)
+print(result_3)
 
 
 # Enter a random number. Save this value to random_number variable
@@ -36,5 +98,23 @@ result_3 = None
 # If a number doesn't meet the first condition, find the remainder of random_value divided by 2.
 # If it is 0, save the text "Even number" to result_4 , else save the message "Odd number"
 
-random_number = None
+random_number = float(input('Enter a random number please: '))
 result_4 = None
+# if 10 > random_number or 99 < random_number:
+#     result_4 = 'Please, put in a number between 10 and 99'
+if random_number < 10 or random_number > 99: # Option 2
+    result_4 = 'Please, put in a number between 10 and 99'
+# elif not 10 > random_number and not 99 < random_number:
+#     result_4 = random_number % 2
+# elif not random_number < 10 and not random_number > 99: # Option 2
+#     result_4 = random_number % 2
+else: # Option 3
+    # if result_4 == 0:               # this one stays with option 1 and option 2 as a root if statement
+    #     result_4 = 'Even number'    # option 1 and 2
+    # else:                           # option 1 and 2
+    #     result_4 = 'Odd number'     # option 1 nad 2
+    if random_number % 2 == 0:
+        result_4 = 'Even number'
+    else:
+        result_4 = 'Odd number'
+print(result_4)
