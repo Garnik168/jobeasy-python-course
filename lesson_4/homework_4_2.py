153db2cd0051dd7b61a7072c4d2443a2530c0e1d
Garnik168 <64793477+Garnik168@users.noreply.github.com>
2020-12-23 08:21:39 -0500

    Homework_4_2_and_4_3_DONE

--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -3,36 +3,71 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = 'Garnik'
+result_1 = ''
 
 # TODO: Here is your code
+for my_name in range(3):
+    result_1 += name_1 + ' '
+print(result_1)
+# times = 3
+# for my_name in range(times):
+#     result_1 = (name_1 + ' ') * 3
+#     print(result_1)
+# print(result_1)
+
+# for my_name in name_1:
+#     result_1 = (my_name + ' ') * 3
+#     print(result_1)
+#
+# for result_1 in name_1:
+#     print(result_1 * 3)
+#
+# result_1 = name_1
+# while result_1 < (name_1 + ' ') * 3:
+#     print(result_1)
+#     result_1 = result_1 + ' ' + name_1
+#
+# repeated_times = 123
+# for my_name in str(repeated_times):
+#     result_1 = name_1
+#     print(result_1)
+
 
 
 # Ex. 2
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
-result_2 = None
+name_2 = input('Enter your Name: ')
+number_1 = input('Enter how many times you want your name to repeat: ')
+result_2 = ''
 
 # TODO: Here is your code
-
+for repeated_name in range(int(number_1)):
+    result_2 += name_2 + ' '
+print(result_2)
 
 # Ex. 3
 # Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
+string_number_1 = input('Enter combination of random numbers that comes up to you mind: ')
+result_3 = 0
 
 # TODO: Here is your code
-
+for digits in string_number_1:
+    result_3 += int(digits)
+print(result_3)
 
 # Ex. 4
 # Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
 
-result_4 = None
+result_4 = 0
 
 # TODO: Here is your code
+for num in range(2, 100):
+    if num % 2 == 0:
+        result_4 += int(num)
+print(result_4)
+
--- lesson_4/homework_4_3.py
+++ lesson_4/homework_4_3.py
@@ -4,20 +4,42 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = 'This is totally random, hopefully long enough string, that contains different characters, that I can work with'
+char_1 = input('Enter the character that you want to count: ')
+result_1 = 0
+current_index = 0
+while current_index < len(string_1):
+    if string_1[current_index] == char_1:
+        result_1 += 1
+    current_index += 1
+else:
+    print(f'Current index is at it\'s last position # {current_index}, the loop has stopped.')
+print(f'The character {char_1} occurs {result_1} times \U0001F913')
 
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
 
-number_1 = None
-result_2 = None
+number_1 = 12345
+result_2 = 1
+index = 0
+
+while index < len(str(number_1)):
+    result_2 = result_2 * int(str(number_1)[index])
+    index += 1
+print(result_2)
 
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = 12345
+result_3 = ''
+index_2 = len(str(number_2)) - 1
+
+while index_2 >= 0:
+    result_3 = str(result_3) + str(number_2)[index_2]
+    index_2 -= 1
+result_3 = int(result_3)
+print(result_3)
+# print(int(result_3)) or just this instead of previous 2 if you don't need to save the result as int
